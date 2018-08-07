47cebe173b1995bfd656a2a5b288253152c5fd88
David Joel McGarvey <davidjmcgarvey@gmail.com>
2018-08-07 11:57:48 -0700

    Added quotes array

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-
+var quotes = [
+  {
+    quote: "Anything is possible!",
+    source: "Kevin Garnet",
+    year: 2008
+  },
+  {
+    quote: "I called game.",
+    source: "Paul Pierce"
+    year: 2015
+  },
+  {
+    quote: "Success is peace of mind which is a direct result of self-satisfaction in knowing you did your best to become the best you are capable of becoming.",
+    source: "John Wooden"
+  }
+  {
+    quote: "Silence is a source of great strength",
+    source: "Lao Tzu"
+  }
+  {
+    quote: "For every minute you remain angry, you give up sixty seconds of peace of mind",
+    source: "Ralph Waldo Emerson"
+  }
+  {
+    quote: "The mediocre teacher tells. The good teacher explains. The superior teacher demonstrates. The great teacher inspires.",
+    source: "William Arthur Ward"
+  }
+];
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
